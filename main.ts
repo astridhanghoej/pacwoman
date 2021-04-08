21630d0dde82e1919257069de117d4c101e355fa
Astrid Hanghøj <astridhanghoej@gmail.com>
2021-04-08 12:11:35 +0200

    Added win condition and parameterized variables

--- main.ts
+++ main.ts
@@ -11,16 +11,31 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSpr
     info.changeScoreBy(1)
     otherSprite.destroy(effects.spray, 500)
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    game.over(false)
+})
+let pacEnemy: Sprite = null
 let pacFood: Sprite = null
 let mySprite: Sprite = null
 let myScore = 0
+let maxScore = 100
 scene.setBackgroundColor(15)
 tiles.setTilemap(tilemap`myTile`)
 mySprite = sprites.create(assets.image`PacWomanRight`, SpriteKind.Player)
-mySprite.setPosition(28, 26)
+tiles.placeOnRandomTile(mySprite, assets.tile`transparency16`)
 controller.moveSprite(mySprite, 50, 50)
 scene.cameraFollowSprite(mySprite)
-for (let index = 0; index < 100; index++) {
+for (let index = 0; index < maxScore; index++) {
     pacFood = sprites.create(assets.image`pacFood`, SpriteKind.Food)
     tiles.placeOnRandomTile(pacFood, assets.tile`transparency16`)
 }
+for (let index = 0; index < 5; index++) {
+    pacEnemy = sprites.create(assets.image`PacEnemy`, SpriteKind.Enemy)
+    tiles.placeOnRandomTile(pacEnemy, assets.tile`transparency16`)
+    pacEnemy.follow(mySprite, 50)
+}
+game.onUpdate(function () {
+    if (info.score() == maxScore) {
+        game.over(true)
+    }
+})
